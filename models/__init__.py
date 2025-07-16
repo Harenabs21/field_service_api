02acf89c57a073bb327a41a28f36152601b558cb
Harenabs21 <hei.harena.6@gmail.com>
2025-07-16 14:46:25 +0300

    create res_users model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import fsm_custom
+from . import res_users
---
+++ models/res_users.py
@@ -0,0 +1,38 @@
+from odoo import models, fields
+import datetime
+import secrets
+
+
+class ResUsers(models.Model):
+
+    _inherit = 'res.users'
+    access_token = fields.Char(string="Access token", readonly=True)
+    token_expiry = fields.Datetime(string="Token Expiry", readonly=True)
+    
+    def generate_access_token(self):
+        token = secrets.token_urlsafe(32)
+        expiry = datetime.datetime.now() + datetime.timedelta(hours=24)
+        
+        self.sudo().write({
+            'access_token': token,
+            'token_expiry': expiry
+        })
+        
+        return token
+    
+    def validate_token(self, token):
+        if not token or not self.access_token:
+            return False
+        
+        if (self.access_token == token and 
+            self.token_expiry and 
+            self.token_expiry > datetime.datetime.now()):
+            return True
+        
+        return False
+    
+    def invalidate_token(self):
+        self.sudo().write({
+            'access_token': False,
+            'token_expiry': False
+        })
