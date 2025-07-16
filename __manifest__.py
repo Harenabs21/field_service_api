fecde9effd52dff8ba1f7e5d2c07816f90a72125
Harenabs21 <hei.harena.6@gmail.com>
2025-07-16 15:00:40 +0300

    update the project name, author name and website

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "fsm_industry_json_rpc",
+    'name': "field_service_api",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "REST API for Field Service Integration in Odoo",
 
     'description': """
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Harena Sarobidy",
+    'website': "https://github.com/Harenabs21",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Industry',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
