998d952ab3d54f5369c409b1b34c668389d0dd02
Harenabs21 <hei.harena.6@gmail.com>
2025-07-22 13:57:46 +0300

    feat: create api_response for api_response class

---
+++ controllers/utils/api_response.py
@@ -0,0 +1,35 @@
+import json
+from datetime import datetime
+from odoo.http import request
+
+
+class ApiResponse:
+
+    @staticmethod
+    def success_response(message, data, status=200):
+        """Formats a success response"""
+        response = {
+            'success': True,
+            'message': message,
+            'data': data,
+            'timestamp': datetime.now().isoformat()
+        }
+        return request.make_response(
+            json.dumps(response, default=str),
+            status=status,
+            headers=[('Content-Type', 'application/json')]
+        )
+
+    @staticmethod
+    def error_response(message, status=400):
+        """Formats an error response"""
+        response = {
+            'success': False,
+            'error': message,
+            'timestamp': datetime.now().isoformat()
+        }
+        return request.make_response(
+            json.dumps(response),
+            status=status,
+            headers=[('Content-Type', 'application/json')]
+        )
