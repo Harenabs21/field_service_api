64e01a5cddccde97bc4979d8dd24eb993e829263
Harenabs21 <hei.harena.6@gmail.com>
2025-07-29 09:32:31 +0300

    create new field and view for signature file

--- models/fsm_custom.py
+++ models/fsm_custom.py
@@ -1,7 +1,10 @@
 from odoo import models, fields
 
+
 class FsmCustom(models.Model):
 
     _inherit = 'project.task'
+
     distance = fields.Char(string="Distance")
-    
+    client_signature = fields.Binary(string="Client Signature")
+    client_signature_filename = fields.Char(string="Signature Filename")
