c00aca3f6921782189d718e78ca6513e58ca79c4
Harenabs21 <hei.harena.6@gmail.com>
2025-07-29 16:28:24 +0300

    create Haversine algorithm to compute the distance between two point

--- models/fsm_custom.py
+++ models/fsm_custom.py
@@ -1,10 +1,47 @@
-from odoo import models, fields
+import math
+from odoo import models, fields, api
 
 
 class FsmCustom(models.Model):
 
     _inherit = 'project.task'
 
-    distance = fields.Char(string="Distance")
+    distance = fields.Float(string="Distance (km)", readonly=True,
+                            compute="_compute_distance")
     customer_signature = fields.Binary(string="Customer Signature")
     customer_signature_filename = fields.Char(string="Signature Filename")
+
+    def _calculate_distance_with_haversine(self, lat1, lon1, lat2, lon2):
+        """
+        Calculate the distance between two geographical points using the
+        Haversine formula.
+        More info here:https://en.wikipedia.org/wiki/Haversine_formula
+        """
+        R = 6371
+        dlat = math.radians(lat2 - lat1)
+        dlon = math.radians(lon2 - lon1)
+        a = math.sin(dlat / 2)**2 + math.cos(math.radians(lat1)) * math.cos(
+            math.radians(lat2)) * math.sin(dlon / 2)**2
+        c = 2 * math.asin(math.sqrt(a))
+        distance = round(R * c, 2)
+        return distance
+
+    @api.depends('partner_id', 'company_id')
+    def _compute_distance(self):
+        for task in self:
+
+            lat1 = task.partner_id.partner_latitude
+            lon1 = task.partner_id.partner_longitude
+
+            lat2 = task.company_id.partner_id.partner_latitude
+            lon2 = task.company_id.partner_id.partner_longitude
+            if (
+                lat1 is not None and
+                lon1 is not None and
+                lat2 is not None and
+                lon2 is not None
+            ):
+                task.distance = task._calculate_distance_with_haversine(
+                    lat1, lon1, lat2, lon2)
+            else:
+                task.distance = 0.0
